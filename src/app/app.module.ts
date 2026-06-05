c91ec4d39b716bdc26a7fbe5e6879fd5c22fa8d5
Zolo517 <191654686+Zolo517@users.noreply.github.com>
2026-06-05 11:32:26 +0800

    feat(backend): added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { TodosModule } from './modules/todos/todos.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, AuthModule, TodosModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
