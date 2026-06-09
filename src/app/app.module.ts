457fff5179c922e87b836145e4d0fbad24938176
Zolo517 <191654686+Zolo517@users.noreply.github.com>
2026-06-09 13:52:10 +0800

    fixed conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-<<<<<<< HEAD
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TodosModule } from './modules/todos/todos.module';
@@ -10,14 +9,5 @@ import { TodosModule } from './modules/todos/todos.module';
   imports: [UsersModule, AuthModule, TodosModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
-=======
-import { UsersModule } from '@/modules/users/users.module';
-import { AuthModule } from '@/modules/auth/auth.module';
-
-@Module({
-  imports: [ UsersModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
->>>>>>> 37940b5 (fixed conflict)
 })
 export class AppModule {}
